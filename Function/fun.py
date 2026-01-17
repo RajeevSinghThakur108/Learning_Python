fe96ade078c2ca11f468c18a98c99aa0e647ac82
“Rajeev <rajeev.thakurdbs@gmail.com>
2026-01-17 10:10:08 +0530

    func

---
+++ Function/fun.py
@@ -0,0 +1,27 @@
+# there 2 type of function 
+#  user define , built in
+
+# User-defined functions → Created by users for specific tasks
+# Built-in functions → Predefined functions available in Python such as max , min , avg , print etc 
+
+# How to Identify User-Defined and Built-in Functions 
+
+# check function defination if it start with def then it is user define or by using type(func_name) , help(func_name)
+
+# function vs method 
+# A function is a block of code that performs a specific task and is not tied to any object.
+# A method is a function that is associated with an object and is called using that object.
+# text = "python"
+# text.upper()
+
+
+
+# funtion defination
+
+def greet(): # def ---> use to create funtion , greet --> name of func () --> to hold perameter / argument : --> finsh the func
+    print("Hello")
+    print("Hi")
+    print("hay")
+
+greet() # calling the func 
+greet()
